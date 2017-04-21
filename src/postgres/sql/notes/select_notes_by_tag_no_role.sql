99413e88c786283293034d957a60944f168660dc
nbena <ciaobena@outlook.it>
2017-04-21 10:41:24 +0200

    Fixed 'no such column'

--- src/postgres/sql/notes/select_notes_by_tag_no_role.sql
+++ src/postgres/sql/notes/select_notes_by_tag_no_role.sql
@@ -1,3 +1,3 @@
-select title, text, isDone, creationDate, lastModficationDate, links
+select title, text, isDone, creationDate, lastModificationDate, links
 from attic.notes join attic.notes_tags on title=noteTitle
 where attic.notes.userId=$1 and tagTitle=$2;
