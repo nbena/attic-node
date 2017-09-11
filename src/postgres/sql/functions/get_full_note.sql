4c712583a0f9d64bb9f09bc5c832a759f9d71e00
nbena <ciaobena@outlook.it>
2017-09-11 21:45:00 +0200

    re written two postgres functions

--- src/postgres/sql/functions/get_full_note.sql
+++ src/postgres/sql/functions/get_full_note.sql
@@ -1,39 +1,86 @@
-create or replace function get_full_note(userId varchar, title varchar) returns json as $$
+-- create or replace function get_full_note(userId varchar, title varchar) returns json as $$
+-- declare
+-- 	noteRes json;
+-- begin
+-- 	noteRes:=  row_to_json(t)
+-- 			from (
+--     			select note.title, note.userId, text, isDone, links, creationDate, lastModificationDate,
+--     				(
+--         				select
+--                         	case
+--                 				when json_agg(d_mainTags) is null then '[]'
+--                 				else json_agg(d_mainTags)
+--                 	end as mainTags
+--         				from (
+--                 			select tagTitle as title
+--                				from attic.notes_tags as rel
+--                 			where rel.userId=$1 and rel.noteTitle=note.title and role='mainTags'
+--                 			order by tagTitle asc
+--                 		) as d_mainTags
+--         			) ,
+--  					(
+--                 		select
+--                             case
+--                 				when json_agg(d_otherTags) is null then '[]'
+--                 				else json_agg(d_otherTags)
+--                 	end as otherTags
+--                 		from (
+--                     		select tagTitle as title
+--                     		from attic.notes_tags as rel
+--                    			where rel.userId=$1 and rel.noteTitle=note.title and role='otherTags'
+--                     		order by tagTitle asc
+--                 		) as d_otherTags
+--              		)
+--     	from attic.notes as note
+--     	where note.userId=$1 and note.title=$2
+--     ) as t;
+--
+--     return noteRes;
+-- end
+-- $$ language plpgsql;
+
+create or replace function get_full_note2(userId varchar, title varchar) returns json as $$
 declare
 	noteRes json;
+    aggOtherTags json;
+    aggMainTags json;
 begin
+
+    aggMainTags := json_agg(main)
+    				from
+                    (
+                    select tagTitle as title
+              			from attic.notes_tags as rel
+              			where rel.userId=$1 and rel.noteTitle=$2 and role='mainTags'
+               			order by tagTitle asc
+                    ) as main;
+
+
+    aggOtherTags := json_agg(other)
+    				from
+                    (
+                    select tagTitle as title
+              			from attic.notes_tags as rel
+              			where rel.userId=$1 and rel.noteTitle=$2 and role='otherTags'
+               			order by tagTitle asc
+                    ) as other;
+
+    if aggMainTags is null then
+    	aggMainTags := '[]'::json;
+     end if;
+
+    if aggOtherTags is null then
+    	aggOtherTags := '[]'::json;
+     end if;
+
+
+
 	noteRes:=  row_to_json(t)
-			from (
-    			select note.title, note.userId, text, isDone, links, creationDate, lastModificationDate,
-    				(
-        				select
-                        	case
-                				when json_agg(d_mainTags) is null then '[]'
-                				else json_agg(d_mainTags)
-                	end as mainTags
-        				from (
-                			select tagTitle as title
-               				from attic.notes_tags as rel
-                			where rel.userId=$1 and rel.noteTitle=note.title and role='mainTags'
-                			order by tagTitle asc
-                		) as d_mainTags
-        			) ,
- 					(
-                		select
-                            case
-                				when json_agg(d_otherTags) is null then '[]'
-                				else json_agg(d_otherTags)
-                	end as otherTags
-                		from (
-                    		select tagTitle as title
-                    		from attic.notes_tags as rel
-                   			where rel.userId=$1 and rel.noteTitle=note.title and role='otherTags'
-                    		order by tagTitle asc
-                		) as d_otherTags
-             		)
-    	from attic.notes as note
-    	where note.userId=$1 and note.title=$2
-    ) as t;
+    from (
+        select *, aggMainTags as mainTags, aggOtherTags as otherTags
+        from attic.notes as n
+        where n.title=$2 and n.userid=$1
+        ) as t;
 
     return noteRes;
 end
--- src/postgres/sql/functions/get_full_tag.sql
+++ src/postgres/sql/functions/get_full_tag.sql
@@ -1,39 +1,70 @@
+-- create or replace function get_full_tag(userId varchar, title varchar) returns json as $$
+-- declare
+-- 	tagRes json;
+-- begin
+-- 	tagRes:=row_to_json(t)
+-- 	from (
+--     	select tag.title, tag.userId,
+--     		(
+--         	select
+--                	case
+--                 	when json_agg(d) is null then '[]'
+--                 	else json_agg(d)
+--                 	end as notes
+--         	from(
+--                 select noteTitle as title
+--                	from attic.notes_tags as rel
+--                 where rel.userId=$1 and rel.tagTitle=tag.title
+--                 order by noteTitle asc
+--                 ) as d
+--         	) ,
+--  			(
+--             select
+-- 							case
+-- 								when json_array_length(json_agg(d)) is null then 0
+-- 								else json_array_length(json_agg(d))
+-- 								end as noteslength
+--             from (
+--             	select noteTitle
+--                 from attic.notes_tags as rel
+--                 where rel.userId=$1 and rel.tagTitle=tag.title
+--                 order by noteTitle asc
+--                 ) as d
+--            )
+--     	from attic.tags as tag
+--     	where tag.userId=$1 and tag.title=$2
+--     ) as t;
+--
+--     return tagRes;
+-- end
+-- $$ language plpgsql;
 create or replace function get_full_tag(userId varchar, title varchar) returns json as $$
 declare
-	tagRes json;
+		tagRes json;
+    notes json;
+    noteslength integer;
 begin
-	tagRes:=row_to_json(t)
-	from (
-    	select tag.title, tag.userId,
-    		(
-        	select
-               	case
-                	when json_agg(d) is null then '[]'
-                	else json_agg(d)
-                	end as notes
-        	from(
+
+    notes := json_agg(notes_row)
+    from (
                 select noteTitle as title
                	from attic.notes_tags as rel
-                where rel.userId=$1 and rel.tagTitle=tag.title
+                where rel.userId=$1 and rel.tagTitle=$2
                 order by noteTitle asc
-                ) as d
-        	) ,
- 			(
-            select
-							case
-								when json_array_length(json_agg(d)) is null then 0
-								else json_array_length(json_agg(d))
-								end as noteslength
-            from (
-            	select noteTitle
-                from attic.notes_tags as rel
-                where rel.userId=$1 and rel.tagTitle=tag.title
-                order by noteTitle asc
-                ) as d
-           )
-    	from attic.tags as tag
-    	where tag.userId=$1 and tag.title=$2
-    ) as t;
+        ) as notes_row;
+
+     if notes is null then
+     	notes := '[]'::json;
+     end if;
+
+     noteslength := json_array_length(notes);
+
+     tagRes:= row_to_json(t)
+     from (
+         select t.title, notes, noteslength
+         from attic.tags as t
+         where t.userid=$1 and t.title=$2
+     ) as t;
 
     return tagRes;
 end
