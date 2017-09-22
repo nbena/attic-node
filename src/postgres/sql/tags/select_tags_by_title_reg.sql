65eefbe759f270853a3c88ae009aed0cc8103459
nbena <ciaobena@outlook.it>
2017-09-22 17:30:37 +0200

    bug fix

--- src/postgres/sql/tags/select_tags_by_title_reg.sql
+++ src/postgres/sql/tags/select_tags_by_title_reg.sql
@@ -1,6 +1,6 @@
 select title, count(tagtitle)::integer as noteslength
 from ${schema~}.tags as t1 left join ${schema~}.notes_tags as nt2 on title=tagtitle and (t1.userId=nt2.userId)
-where t.userid=$1 and t.title like $2
+where t1.userid=$1 and t1.title like $2
 and (t1.userid=nt2.userid or nt2.userid is null)
-group by title, t.userId
+group by title, t1.userId
 order by noteslength desc, title asc;
